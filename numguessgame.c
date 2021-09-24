c72098fde3b366a56caa5dd4f9025b7616412507
Daniel Madrigal <dmadrigal@cpp.edu>
2021-09-24 01:01:10 -0700

    Add files

---
+++ numguessgame.c
@@ -0,0 +1,6 @@
+/*include stdio.h, stdlib.h, and time.h*/
+/*Create main*/
+/* Declare and initialize max number*/
+/* Declare variables for the menu options, random number, the users guess */
+/*Print menu options*/
+/* Allow user to leave during the game*/
---
+++ restaurantbill.c
@@ -0,0 +1,8 @@
+/*include stdio.h, stdlib.h, and time.h*/
+/*Create main*/
+/*Declare and init variables for the food cost*/
+/* Declare tax, tip, and total variables*/
+/*Create a function to randomly choose the food option*/
+/* Ask user through command line for tax and tip percentage*/
+/*Create a function to calculate tax, tip, then one for total*/
+/*print meal cost, tax amount, tip amount, and total*/
